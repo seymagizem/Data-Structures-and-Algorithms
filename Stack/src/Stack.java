d2848473d64a7a08ce74c5591f5822bfb7d5f2ed
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-20 01:57:01 +0300

    Add files via upload

---
+++ Stack/src/Stack.java
@@ -0,0 +1,59 @@
+
+public class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full!");
+            return;
+        }
+        stackArray[++top] = value;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+}
+
+Stack stack = new Stack(3);
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+System.out.println(stack.peek()); //prints 3
+
+stack.pop();
+
+while (!stack.isEmpty()) {
+    System.out.print(stack.pop() + " "); //prints 2 1
+}
+

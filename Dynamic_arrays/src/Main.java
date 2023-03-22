0b39465a59591699a6e9f163ee926542c0ab4eda
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-22 15:58:48 +0300

    Update Main.java

--- Dynamic_arrays/src/Main.java
+++ Dynamic_arrays/src/Main.java
@@ -1,3 +1,5 @@
+//In this example, we create an ArrayList of Strings and add three elements to it using the add() method. The println() method is used to print the entire list to the console.
+
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {

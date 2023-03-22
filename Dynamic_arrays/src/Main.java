4a075bb8f7c2c0bcc0e4879d7a20fb1f93324e74
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-22 15:58:12 +0300

    Add files via upload

---
+++ Dynamic_arrays/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+        System.out.println(list); // prints [apple, banana, orange]
+
+    }
+}
